a8adb7e1b5b8fb0eae31c96609317a23af198d32
Javiehz <javier.encher@educa.jcyl.es>
2025-11-09 17:23:18 +0100

    1 - Save location on SharedPreferences

--- app/src/main/kotlin/com/weatherApp/utils/PreferencesManager.kt
+++ app/src/main/kotlin/com/weatherApp/utils/PreferencesManager.kt
@@ -6,11 +6,27 @@ import android.content.SharedPreferences
 class PreferencesManager(context: Context) {
 
     //TODO get sharedPreferences file
+    val sharedPrefs =
+        context.getSharedPreferences("sharedPreferencesFile", Context.MODE_PRIVATE)
 
     //TODO save data in SharedPreferences
-    fun saveLocation(city: String, latitude: Double, longitude: Double) {}
+    fun saveLocation(city: String, latitude: Double, longitude: Double) {
+        with(sharedPrefs.edit()) {
+            putString("city", city)
+            putFloat("latitude", latitude.toFloat())
+            putFloat("longitude", longitude.toFloat())
+            apply()
+        }
+    }
     //TODO implement getters for SharedPreferences values, using mock data by now
-    fun getCityName(): String? = "Ávila"
-    fun getLatitude(): Float? = 40.6572f
-    fun getLongitude(): Float? = -4.6995f
+
+    fun getCityName(): String? {
+        return sharedPrefs.getString("city", "No Hay")
+    }
+    fun getLatitude(): Float? {
+        return sharedPrefs.getFloat("latitude", 0.0f)
+    }
+    fun getLongitude(): Float? {
+        return sharedPrefs.getFloat("longitude", 0.0f)
+    }
 }
